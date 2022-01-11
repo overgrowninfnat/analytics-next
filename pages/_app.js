26f9f6839f406c875867272ea360f07b0dce3c9f
theItalianDev <theItalianDev@MinidiGiuseppe.wind3.hub>
2022-01-11 19:40:17 +0100

    google analytics code added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`} strategy='afterInteractive' />
+      <Script id="google-analytics-script" strategy='afterInteractive'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  ) 
 }
 
 export default MyApp
