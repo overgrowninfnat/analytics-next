0c7819000b10b39aaf9b811724013c02e028fb55
theItalianDev <theItalianDev@MinidiGiuseppe.wind3.hub>
2022-01-13 23:06:09 +0100

    about page added

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,10 @@
+import Head from 'next/head'
 export default function About() {
     return(
         <div>
+            <Head>
+                <title>About page</title>
+            </Head>
             <h1>About page</h1>
         </div>
     )
