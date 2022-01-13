17fbbd322fab3a9bca71b9377d0a95db65946f6d
theItalianDev <theItalianDev@MinidiGiuseppe.wind3.hub>
2022-01-13 23:04:42 +0100

    about page added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <Link href="/about"><a>About Page</a>/Link>
 
         <h2 className={styles.description}>
           Get started by editing{' '}
